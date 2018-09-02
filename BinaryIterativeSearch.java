4c6d40c6c84be00d950fa39bcc5b79023550fcda
OrshiKozek <okozek@dons.usfca.edu>
2018-09-01 23:14:13 -0700

    Fixed and updated

--- BinaryIterativeSearch.java
+++ BinaryIterativeSearch.java
@@ -10,18 +10,20 @@ public class BinaryIterativeSearch implements Practice2Search {
         int min = 0;
         int max = arr.length;
         int mid = (min + max) / 2;
-        while (min < max) {
-            if (mid == target)
+        while (min <= max) {
+            if (arr[mid] == target) {
                 return mid;
-            else if (mid < target) { //the target is in the upper half // }
+            }
+            else if (arr[mid] < target) { //the target is in the upper half // }
                 min = mid + 1; //sets the min to one plus the mid
                 mid = (min + max) / 2; //sets mid to the middle of the 'new', smaller array
                 //max stays the same
             }
-            else //the last option is if the target is below the middle
+            else {//the last option is if the target is below the middle
                 max = mid - 1; //sets the max to be the one less than the mid
-                // mid = (min + max) / 2; //changes the mid to the middle of the 'new', smaller array
+                mid = (min + max) / 2; // mid = (min + max) / 2; //changes the mid to the middle of the 'new', smaller array
                 //min is not changed
+            }
         }
         return -1; //if the target is never found, then return -1
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CS 245 (Fall, 2018) PracticeAssignment02
 
 See assignment details on Canvas.
+
+Practice2Test.java does not run. It displays this message on the console: Could not dins or load main class Practice2Tests.java
+
+However, the necessary files are added, and they work.
